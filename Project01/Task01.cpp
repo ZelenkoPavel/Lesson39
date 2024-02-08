70ba0210482dedeb0a9135d4886af829b424585f
ITSTEP\st <st>
2024-02-08 17:32:47 +0300

    execution speed check

--- Project01/Task01.cpp
+++ Project01/Task01.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
-string prime_number(int);
+string is_prime_number1(int);
+string is_prime_number2(int);
 
 int main() {
 
 	int number; 
 
 	cout << "Input number: ";
-	cin >> number;
+    cin >> number;
+	float start_1 = time(NULL);
+
+	cout << "Result 1 - " << is_prime_number1(number)  << endl;
+
+	float end_1 = time(NULL);
+
+	cout << "Time - " << end_1 - start_1 << endl;
 
 
-	cout << "Result - " << prime_number(number)  << endl;
+
+
+	float start_2 = time(NULL);
+
+	cout << "Result 2 - " << is_prime_number2(number) << endl;
+
+	float end_2 = time(NULL);
+
+	cout << "Time - " << end_2 - start_2 << endl;
 
 	return 0;
 }
--- Project01/logic.cpp
+++ Project01/logic.cpp
@@ -3,13 +3,27 @@
 
 using namespace std;
 
-string prime_number(int number) {
+string is_prime_number1(int number) {
 	if (number <= 2) {
 		return "-1";
 	}
 
-	for (int i = 2; i < number / 2; i++) {
-		if (number / i != 1 && number % i == 0) {
+	for (int divider = 2; divider < number; divider++) {
+		if (number / divider != 1 && number % divider == 0) {
+			return "No";
+		}
+	}
+	return "Yes";
+}
+
+
+string is_prime_number2(int number) {
+	if (number <= 2) {
+		return "-1";
+	}
+
+	for (int divider = 2; divider < number / 2; divider++) {
+		if (number / divider != 1 && number % divider == 0) {
 			return "No";
 		}
 	}
